3c5a6cac3a103c647e93518f9f64712ab4fde513
Jonathan Como <jonathan.como@gmail.com>
2016-04-14 19:11:50 -0700

    Fix bug with LenientDict not modifying underlying dict

--- snake/datastructures.py
+++ snake/datastructures.py
@@ -1,9 +1,33 @@
-class LenientDict(dict):
+from collections import MutableMapping
+
+
+class LenientDict(MutableMapping):
     """A class describing a dict that does not raise when using bracket notation on a key that
-    does not exist. Behaves the same as a regular dict otherwise.
+    does not exist. Implements the core dict interface.
     """
+    def __init__(self, dct=None):
+        self._dict = dct or {}
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __contains__(self, key):
+        return key in self._dict
+
     def __getitem__(self, key):
         try:
-            return super(LenientDict, self).__getitem__(key)
+            return self._dict[key]
+        except KeyError:
+            pass
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+
+    def __delitem__(self, key):
+        try:
+            del self._dict[key]
         except KeyError:
             pass
--- tests/datastructures_tests.py
+++ tests/datastructures_tests.py
@@ -4,16 +4,35 @@ from snake.datastructures import LenientDict
 
 
 class LenientDictTestCase(TestCase):
+
+    def test_it_modifies_the_underlying_dict(self):
+        base = {'a': 1}
+
+        d = LenientDict(base)
+        d['a'] = 2
+
+        self.assertEqual(2, d['a'])
+        self.assertEqual(2, base['a'])
+
     def test_it_acts_like_dict(self):
         d = LenientDict({'a': 1})
         d['b'] = 2
 
+        self.assertEqual(2, len(d))
         self.assertEqual(1, d['a'])
         self.assertEqual(2, d['b'])
         self.assertTrue('a' in d)
         self.assertFalse('c' in d)
 
+        for key in d:
+            pass
+
     def test_it_returns_nothing_instead_of_raising_for_key_error(self):
         d = LenientDict()
 
         self.assertIsNone(d['a'])
+
+    def test_it_deletes_without_raising_key_error(self):
+        d = LenientDict()
+
+        del d['a']
